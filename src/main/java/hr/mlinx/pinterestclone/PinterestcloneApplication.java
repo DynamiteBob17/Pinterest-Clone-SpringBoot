f1566895f49fbb5b0727c69345468233132ad967
PickleEaterJim33 <jan.mlinaric@gmail.com>
2023-06-05 21:10:01 +0200

    Initial commit

---
+++ src/main/java/hr/mlinx/pinterestclone/PinterestcloneApplication.java
@@ -0,0 +1,13 @@
+package hr.mlinx.pinterestclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinterestcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinterestcloneApplication.class, args);
+    }
+
+}
