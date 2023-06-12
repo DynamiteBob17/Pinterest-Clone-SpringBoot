fb556881360d0a3a7abcd0a26feee3b54b46cfd3
PickleEaterJim33 <jan.mlinaric@gmail.com>
2023-06-12 22:10:40 +0200

    chore: extract default imageUrl as a constant

---
+++ src/main/java/hr/mlinx/pinterestclone/util/Defaults.java
@@ -0,0 +1,7 @@
+package hr.mlinx.pinterestclone.util;
+
+public final class Defaults {
+
+    public static final String IMAGE_URL = "https://i.kym-cdn.com/entries/icons/original/000/031/015/cover5.jpg";
+
+}
