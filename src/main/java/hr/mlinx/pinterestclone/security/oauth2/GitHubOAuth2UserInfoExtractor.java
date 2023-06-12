21fc6079f4ab759a3d767b40dbea15a8bb9ca3a4
PickleEaterJim33 <jan.mlinaric@gmail.com>
2023-06-12 22:12:58 +0200

    fix: don't break when no access to GitHub profile photo or email

--- src/main/java/hr/mlinx/pinterestclone/security/oauth2/GitHubOAuth2UserInfoExtractor.java
+++ src/main/java/hr/mlinx/pinterestclone/security/oauth2/GitHubOAuth2UserInfoExtractor.java
@@ -4,6 +4,7 @@ import hr.mlinx.pinterestclone.model.AuthProvider;
 import hr.mlinx.pinterestclone.model.Role;
 import hr.mlinx.pinterestclone.model.RoleName;
 import hr.mlinx.pinterestclone.security.CustomUserDetails;
+import hr.mlinx.pinterestclone.util.Defaults;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -16,9 +17,15 @@ public class GitHubOAuth2UserInfoExtractor implements OAuth2UserInfoExtractor {
     @Override
     public CustomUserDetails extractUserInfo(OAuth2User oAuth2User) {
         CustomUserDetails customUserDetails = new CustomUserDetails();
-        customUserDetails.setUsername(retrieveAttr("login", oAuth2User));
-        customUserDetails.setEmail(retrieveAttr("email", oAuth2User));
-        customUserDetails.setImageUrl(retrieveAttr("avatar_url", oAuth2User));
+        String login;
+        customUserDetails.setUsername(login = retrieveAttr("login", oAuth2User));
+
+        String email = retrieveAttr("email", oAuth2User);
+        customUserDetails.setEmail(email.isBlank() ? (login + "@" + login) : email);
+
+        String imageUrl = retrieveAttr("avatar_url", oAuth2User);
+        customUserDetails.setImageUrl(imageUrl.isBlank() ? Defaults.IMAGE_URL : imageUrl);
+
         customUserDetails.setAuthProvider(AuthProvider.GITHUB);
         customUserDetails.setAttributes(oAuth2User.getAttributes());
         Role role = new Role();
