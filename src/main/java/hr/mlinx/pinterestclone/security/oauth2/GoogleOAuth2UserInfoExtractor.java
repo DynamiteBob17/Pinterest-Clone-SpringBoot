380786d1076db1f3644f399dd1e5faf74ad54c75
PickleEaterJim33 <jan.mlinaric@gmail.com>
2023-06-12 22:12:37 +0200

    fix: don't break when no access to google profile photo

--- src/main/java/hr/mlinx/pinterestclone/security/oauth2/GoogleOAuth2UserInfoExtractor.java
+++ src/main/java/hr/mlinx/pinterestclone/security/oauth2/GoogleOAuth2UserInfoExtractor.java
@@ -4,6 +4,7 @@ import hr.mlinx.pinterestclone.model.AuthProvider;
 import hr.mlinx.pinterestclone.model.Role;
 import hr.mlinx.pinterestclone.model.RoleName;
 import hr.mlinx.pinterestclone.security.CustomUserDetails;
+import hr.mlinx.pinterestclone.util.Defaults;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,10 @@ public class GoogleOAuth2UserInfoExtractor implements OAuth2UserInfoExtractor {
         CustomUserDetails customUserDetails = new CustomUserDetails();
         customUserDetails.setUsername(retrieveAttr("email", oAuth2User));
         customUserDetails.setEmail(retrieveAttr("email", oAuth2User));
-        customUserDetails.setImageUrl(retrieveAttr("picture", oAuth2User));
+
+        String imageUrl = retrieveAttr("picture", oAuth2User);
+        customUserDetails.setImageUrl(imageUrl.isBlank() ? Defaults.IMAGE_URL : imageUrl);
+
         customUserDetails.setAuthProvider(AuthProvider.GOOGLE);
         customUserDetails.setAttributes(oAuth2User.getAttributes());
         Role role = new Role();
